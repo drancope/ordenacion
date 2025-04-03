78e7b0019268173fac2a7e7fb6bdb11a56f3d95e
drancope <drancope@gmail.com>
2025-04-03 13:03:53 +0200

    reducido un loop

--- tres_valores.py
+++ tres_valores.py
@@ -5,11 +5,11 @@
 
 n = []
 
-for i in range(3):
+for i in range(6):
     n.append(int(input()))
 
 s = n
-for i in range(2):    #Aquí se refleja el error p.ej. con [7, 5, 3]
+for i in range(5):    #Aquí se refleja el error p.ej. con [7, 5, 3]
     if s[i] > s[i+1]:
         a = s[i]
         s[i] = s[i+1]
@@ -17,7 +17,7 @@ for i in range(2):    #Aquí se refleja el error p.ej. con [7, 5, 3]
 print(s)
 
 s = n
-for i in range(len(n)-1):   #Aquí hacemos el repaso a la lista dos veces
+for i in range(len(n)-2):   #Aquí hacemos el repaso a la lista dos veces
     for j in range(len(n)-1):
         if s[j] > s[j+1]:
             a = s[j]
